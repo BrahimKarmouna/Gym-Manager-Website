366d8376576e3f7399a8405a68ff0509eaf7caf1
Brahim karmouna <b.karmouna@ADSGLORYIT.ma>
2024-10-29 10:44:06 +0100

    crete emojie section in transaction category addition

--- app/Models/TransactionCategory.php
+++ app/Models/TransactionCategory.php
@@ -1,12 +1,15 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TransactionCategory extends Model
 {
-  protected $fillable = [
-    "name"
-  ];
+    use HasFactory;
+
+    protected $fillable = [
+      'name',
+      'emoji'
+    ];
 }
--- resources/js/pages/transaction-categories/IndexPage.vue
+++ resources/js/pages/transaction-categories/IndexPage.vue
@@ -1,4 +1,4 @@
-<template  >
+<template>
 
 
   <!-- <ul>
@@ -17,39 +17,43 @@
   </ul> -->
   <section class="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-16">
     <q-btn :to="{ name: 'transaction-categories.create' }" color="primary" icon="sym_r_add" label="Create" />
-  <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
-    <div class="mb-4 flex items-center justify-between gap-4 md:mb-8">
-      <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Shop by category</h2>
+    <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+      <div class="mb-4 flex items-center justify-between gap-4 md:mb-8">
+        <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Shop by category</h2>
 
-      <a href="#" title="" class="flex items-center text-base font-medium text-primary-700 hover:underline dark:text-primary-500">
-        See more categories
-        <svg class="ms-1 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
-        </svg>
-      </a>
+        <a href="#" title=""
+          class="flex items-center text-base font-medium text-primary-700 hover:underline dark:text-primary-500">
+          See more categories
+          <svg class="ms-1 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+            fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M19 12H5m14 0-4 4m4-4-4-4" />
+          </svg>
+        </a>
 
-    </div>
+      </div>
 
-    <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      <a
-      v-for="transactionCategory in data"
-      :key="transactionCategory.id"
-      href="#"
-      class="flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-      @click.prevent="$router.push({ name: 'transaction-categories.edit', params: { id: transactionCategory.id } })"
-    >
-      <svg class="me-2 h-4 w-4 shrink-0 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <a v-for="transactionCategory in data" :key="transactionCategory.id" href="#"
+          class="flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+          @click.prevent="$router.push({ name: 'transaction-categories.edit', params: { id: transactionCategory.id } })">
+          <!-- <svg class="me-2 h-4 w-4 shrink-0 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v5m-3 0h6M4 11h16M5 15h14a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1Z"></path>
-      </svg>
-      <span class="text-sm font-medium text-gray-900 dark:text-white">
-        {{ transactionCategory.name }}
-      </span>
-      <q-btn flat color="primary" icon="delete" @click.stop="deleteItem(transactionCategory)" />
-    </a>
+      </svg> -->
 
+          <img v-if="transactionCategory.emoji"
+            :src="`https://cdn.jsdelivr.net/npm/emoji-datasource-apple@6.0.1/img/apple/64/${transactionCategory.emoji}.png`"
+            class="me-2 w-6">
+
+          <span class="text-sm font-medium text-gray-900 dark:text-white">
+            {{ transactionCategory.name }}
+          </span>
+          <q-btn flat color="primary" icon="delete" @click.stop="deleteItem(transactionCategory)" />
+        </a>
+
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 </template>
 
 <script setup>
