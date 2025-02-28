c5baf1e159aa0f74cae3ffb66e209b4ebec6ec4c
Brahim karmouna <b.karmouna@ADSGLORYIT.ma>
2025-02-28 13:11:19 +0000

    composer updates

--- resources/js/middlewares/VerifyMiddleware.js
+++ resources/js/middlewares/VerifyMiddleware.js
@@ -7,6 +7,12 @@ export async function VerifyMiddleware({ store, to, next }) {
 
   const user = authStore.user;
 
+  // If there is no user, redirect to the registration page
+  if (!user) {
+    return next({ name: "register" }); // Adjust route name based on your setup
+  }
+
+  // If the user's email is not verified, redirect to profile
   if (user?.email_verified_at === null) {
     return next({ name: "profile.index" });
   }
